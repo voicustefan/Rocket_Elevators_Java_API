9e219b89842347290a32c4d41dcc0ffc81d4f638
Stefan Voicu <voicu.stefan@courrier.uqam.ca>
2022-12-21 11:40:31 -0500

    Initial commit

---
+++ src/test/java/com/rocket/elevators/ElevatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.elevators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
